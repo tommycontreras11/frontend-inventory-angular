683ce36f96995ce6658ba566180e86c6adbb92b4
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-11-24 10:23:45 -0400

    feat: :sparkles: add component to create articles

---
+++ src/app/articles/create/create-article.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { createArticle, IArticle } from '../../../api/article';
+
+@Component({
+  selector: 'app-create-article',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './create-article.component.html',
+  styleUrl: './../../../styles.css',
+})
+export class CreateArticleComponent {
+  title = 'frontend-inventory-angular';
+
+  articleForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.articleForm = this.fb.group({
+      description: [''],
+      entryDate: [''],
+      expiryDate: [''],
+      quantity: [0],
+      cost: [0]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.articleForm.valid) {
+      const createArticleObject: IArticle = {
+        ...this.articleForm.value,
+      };
+
+      createArticle(createArticleObject).then(
+        response => {
+          console.log('Article created successfully:', response);
+        },
+        error => {
+          console.error('Error creating article:', error);
+        }
+      );
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
