9709db24b20bfcfcc325d900fbc6f1c2ef6a5e33
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-11-24 10:21:57 -0400

    feat: :sparkles: add file to organize routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ArticleComponent } from './articles/article.component';
+import { HomeComponent } from './home/home.component';
+import { UpdateArticleComponent } from './articles/edit/update-article.component';
+import { CreateArticleComponent } from './articles/create/create-article.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },  
+  { path: 'home', component: HomeComponent },  
+  { path: 'articles', component: ArticleComponent },  
+  { path: 'articles/create', component: CreateArticleComponent },
+  { path: 'articles/edit/:id', component: UpdateArticleComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }  
+];
