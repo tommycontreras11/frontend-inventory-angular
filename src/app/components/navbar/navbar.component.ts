589c48a8043e2eeb8a6bda518992fb6aec361194
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-11-24 10:20:47 -0400

    feat: :sparkles: add navbar component to navigate through routes

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./../../../styles.css'],
+})
+export class NavbarComponent {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+}
