c258d1a6460d244039a792830839338da4e6d87c
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2024-11-24 10:23:02 -0400

    feat: :sparkles: add navbar to app component to act like a layout and retrieve the navbar in every page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IArticle } from '../api/article';
+import { retrieveArticles } from '../services/article';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './../styles.css',
 })
 export class AppComponent {
   title = 'frontend-inventory-angular';
